08bda312efb195d765f6bf400ed523af50f2cc18
Dominik Magic <dominikmagic5@gmail.com>
2023-01-20 09:55:55 -0800

    Cat  model added

---
+++ AnimalShelterApi/Models/Cat.cs
@@ -0,0 +1,10 @@
+namespace AnimalShelterApi.Models
+{
+  public class Cat
+  {
+    public int CatId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+  }
+}
