62997a684c1e85d9d75f4d1d431a9c7373ed75a8
Dominik Magic <dominikmagic5@gmail.com>
2023-01-20 11:12:56 -0800

    seeding data for cats table added

--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -20,6 +20,12 @@ namespace AnimalShelterApi.Models
           new Dog { DogId = 2, Name = "Wolfie", Breed = "Husky", Age = 7 },
           new Dog { DogId = 3, Name = "Craig", Breed = "Pit Bull", Age = 5 }
         );
+      builder.Entity<Cat>()
+        .HasData(
+          new Cat { CatId = 1, Name = "Fluffy", Breed = "Persian", Age = 4 },
+          new Cat { CatId = 2, Name = "Missy", Breed = "Siamese", Age = 2 },
+          new Cat { CatId = 3, Name = "Nala", Breed = "Ragdoll", Age = 9 }
+        );
     }
     
   }
