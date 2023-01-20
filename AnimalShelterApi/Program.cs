e25ec19c7944a20f4c9db71e29ba078fd41c9742
Dominik Magic <dominikmagic5@gmail.com>
2023-01-20 15:56:26 -0800

    WIP: working on implementing API versioning

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -16,6 +16,15 @@ builder.Services.AddDbContext<AnimalShelterApiContext>(
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
 
 var app = builder.Build();
 
