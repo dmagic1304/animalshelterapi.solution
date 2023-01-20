78fed2c467149c0c21a817b4724bca857eb2826b
Dominik Magic <dominikmagic5@gmail.com>
2023-01-20 09:30:32 -0800

    Dog model added

---
+++ AnimalShelterApi/Models/Dog.cs
@@ -0,0 +1,10 @@
+namespace AnimalShelterApi.Models
+{
+  public class Dog
+  {
+    public int DogId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+  }
+}
